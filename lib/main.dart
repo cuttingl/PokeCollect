3fb611f49778a707e5bd5b354d7537f24bbe7764
laurent <laurent.cutting@gmail.com>
2024-10-08 04:07:50 +0200

    get one pokemon card and display its image with attacks // default image added (LOTS OF TODOS)

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,76 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:pokemon_tcg/pokemon_tcg.dart';
+import 'env.dart';
 
-void main() => runApp(const MyApp());
+/// Flutter code sample for [Checkbox].
 
+void main() => runApp(const CheckboxExampleApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CheckboxExampleApp extends StatelessWidget {
+  const CheckboxExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('PokeCollect with a simple example')),
+        appBar: AppBar(title: const Text('Checkbox Sample')),
         body: const Center(
-          child: HomePage(),
+          child: CheckboxExample(),
         ),
       ),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class CheckboxExample extends StatefulWidget {
+  const CheckboxExample({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<CheckboxExample> createState() => _CheckboxExampleState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _CheckboxExampleState extends State<CheckboxExample> {
+  bool isChecked = false;
+  PokemonCard? card;
+  Image image = Image.network(
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1msMCg.img");
+  String text = "";
+
+  Future<PokemonCard?> getApi() async {
+    final api = PokemonTcgApi(apiKey: APIKEY);
+    card = await api.getCard('sv6-3');
+    return card;
+  }
+
   @override
   Widget build(BuildContext context) {
-    bool? isChecked = false;
-
-    return Checkbox(
-        tristate: true,
-        value: isChecked,
-        onChanged: (value) {
-          setState(() {
-            isChecked = value;
-          });
-        });
+    getApi();
+    return SelectionArea(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Center(
+        child: Checkbox(
+          checkColor: Colors.white,
+          value: isChecked,
+          onChanged: (bool? value) {
+            setState(() async {
+              if (!isChecked) {
+                image = Image.network(card!.images.small);
+                text = StringBuffer([card!.name, card!.artist, card!.attacks.first.name ]).toString();
+                isChecked = value!;
+              } else if (isChecked) {
+                image = Image.network(
+                    "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1msMCg.img");
+                isChecked = value!;
+              }
+            });
+          },
+        ),
+      ),
+      Text(text),
+      Image(image: image.image),
+    ]));
   }
 }
